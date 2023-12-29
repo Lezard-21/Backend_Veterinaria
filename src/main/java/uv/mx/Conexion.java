5084d863892293ad529a4f6247fe2d82a57665fa
David Vega Tepetla <pescador2111@hotmail.com>
2023-12-29 14:24:11 -0600

    se modifico el jar

--- src/main/java/uv/mx/Conexion.java
+++ src/main/java/uv/mx/Conexion.java
@@ -5,10 +5,10 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class Conexion {
-    private static String url = "jdbc:mysql://127.0.0.1:3306/veterinaria";
+    private static String url = "jdbc:"+getAssignedurl();
     private static String driverName = "com.mysql.cj.jdbc.Driver"; //"com.mysql.jdbc.Driver"; // com.mysql.cj.jdbc.Driver
-    private static String username = "root";
-    private static String password = "Loldavid2111&";
+    private static String username = getAssignedUser();
+    private static String password = getAssignedPassword();
     // variable de conexion
     private static Connection connection = null;
 
@@ -22,5 +22,26 @@ public class Conexion {
             System.out.println("Driver:" + e);
         }
         return connection;
+    }
+    static String getAssignedUser() {
+        ProcessBuilder processBuilder = new ProcessBuilder();
+        if (processBuilder.environment().get("MYSQLUSER") != null) {
+            return processBuilder.environment().get("MYSQLUSER");
+        }
+        return "root";
+    }
+    static String getAssignedPassword() {
+        ProcessBuilder processBuilder = new ProcessBuilder();
+        if (processBuilder.environment().get("MYSQL_ROOT_PASSWORD") != null) {
+            return processBuilder.environment().get("MYSQL_ROOT_PASSWORD");
+        }
+        return "Loldavid2111&";
+    }
+    static String getAssignedurl() {
+        ProcessBuilder processBuilder = new ProcessBuilder();
+        if (processBuilder.environment().get("MYSQL_URL") != null) {
+            return processBuilder.environment().get("MYSQL_URL");
+        }
+        return "mysql://127.0.0.1:3306/veterinaria";
     }
 }
