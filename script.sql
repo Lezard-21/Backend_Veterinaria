340a86361ad7bdcc53a921b201eb00f5f5493036
David Vega Tepetla <pescador2111@hotmail.com>
2023-12-28 23:06:40 -0600

    se arreglo un error

--- script.sql
+++ script.sql
@@ -1,15 +1,9 @@
 #show databases;
 create database veterinaria;
+CREATE USER yo IDENTIFIED BY '123';
+GRANT ALL PRIVILEGES ON veterinaria.* TO 'yo'@'%';
 use veterinaria;
 #show tables;
-create table animal (
-					id_animal varchar(255) primary key,
-                    especie varchar(255) not null,
-                    nombre varchar(255),
-                    motivo varchar(255),
-                    id_dueño varchar(255),
-                    foreign key (id_dueño) references dueño(id_dueño)
-                    );
 create table cliente (
 					 id_cliente varchar(255) primary key,
                      nombre varchar(255) not null,
@@ -20,7 +14,14 @@ create table dueño (
                      nombre varchar(255) not null,
                      telefono varchar(255) not null
                      );
-
+create table animal (
+					id_animal varchar(255) primary key,
+                    especie varchar(255) not null,
+                    nombre varchar(255),
+                    motivo varchar(255),
+                    id_dueño varchar(255),
+                    foreign key (id_dueño) references dueño(id_dueño)
+                    );
 DELIMITER $$
 CREATE TRIGGER delete_related_animals 
 BEFORE DELETE ON dueño 
@@ -30,9 +31,9 @@ BEGIN
 END$$
 DELIMITER ;
 
-insert into dueño (id_dueño,nombre,telefono) values ("1234","david","12312312");
-insert into animal (id_animal,especie,nombre,id_dueño) values ("1234ssadas","gato","jjj","1234");
-insert into cliente (id_cliente,nombre,contraseña) values ("12345","david","12345");
+#insert into dueño (id_dueño,nombre,telefono) values ("1234","david","12312312");
+#insert into animal (id_animal,especie,nombre,id_dueño) values ("1234ssadas","gato","jjj","1234");
+#insert into cliente (id_cliente,nombre,contraseña) values ("12345","david","12345");
 
 #drop trigger delete_related_animals;
 
@@ -46,5 +47,5 @@ insert into cliente (id_cliente,nombre,contraseña) values ("12345","david","123
 #select * from animal;
 #select * from cliente;
 #select * from dueño;
-
+#SELECT * from cliente where nombre="david" and contraseña="12345";
 #delete from dueño where id_dueño = "1234"; 
--- src/main/java/uv/mx/App.java
+++ src/main/java/uv/mx/App.java
@@ -25,21 +25,7 @@ public class App
         // get("/clientes/byId", ClienteApi.buscarPorId);
         // post("/clientes", ClienteApi.agregar);
         // delete("/clientes/:id", ClienteApi.eliminar);
-        // patch("/clientes", ClienteApi.modificar);
-        
-        options("/*",(request,response)->{
-        String accessControlRequestHeaders=request.headers("Access-Control-Request-Headers");
-        if(accessControlRequestHeaders!=null){
-        response.header("Access-Control-Allow-Headers",accessControlRequestHeaders);
-        }
-        String accessControlRequestMethod=request.headers("Access-Control-Request-Method");
-        if(accessControlRequestMethod!=null){
-        response.header("Access-Control-Allow-Methods",accessControlRequestMethod);
-        }
-        return "OK";
-        });
-        before((request,response)->response.header("Access-Control-Allow-Origin","*"));
-            
+        // patch("/clientes", ClienteApi.modificar); 
         path("/api", () -> {
             before("/*", (q, a) -> System.out.println("Received api call"));
             path("/clientes", () -> {
